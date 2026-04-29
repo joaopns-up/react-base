3afe86dec83ec8f6451d884b47a8fe2bc538160f
João Paulo Nunes da Silva <nunes0188@outlook.com.br>
2026-04-29 20:14:31 -0300

    Finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
-
+import { CategoriasPage } from './pages/CategoriasPage';
+import { HomePage } from './pages/HomePage';
+import { LoginPage } from './pages/LoginPage';
+import { ProdutosPage } from './pages/ProdutosPage';
+import { RegisterPage } from './pages/RegisterPage';
+import { RequireAuth } from './routes/RequireAuth';
+import { Layout } from './ui/Layout';
 
 function App() {
   return (
     <Routes>
-      <Route>
-
+      <Route element={<Layout />}>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route
+          path="/categorias"
+          element={
+            <RequireAuth>
+              <CategoriasPage />
+            </RequireAuth>
+          }
+        />
+        <Route
+          path="/produtos"
+          element={
+            <RequireAuth>
+              <ProdutosPage />
+            </RequireAuth>
+          }
+        />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
